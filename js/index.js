a5d67e442592c4826e30de6d071b453cb5bd766b
gksqlc40 <gksqlc40@gmail.com>
2020-07-20 18:19:50 +0900

    add_css__200720

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 "use strict";
-const boxForm = document.querySelector(".userBox");
+const boxForm = document.querySelector(".user__box");
 const userNumber = document.querySelector("#input");
 
 function input(event) {
   event.preventDefault();
   const userValue = userNumber.value;
-  if (userValue == 1) {
-    location.href = "student_1.html";
+  if (userValue == 6500) {
+    location.href = "dbsgusrud6500.html";
   } else {
     alert("User 정보가 없습니다.");
   }
